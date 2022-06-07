5c08175b19367ca7ca63b3f772bd32cd14718002
riqueschilder <99187952+riqueschilder@users.noreply.github.com>
2022-06-07 07:54:46 -0300

    Semana7

--- 03_AUT_EST_ENTREGA/Semana 1/Frontend/Curriculo/script.js
+++
@@ -1,1 +0,0 @@
-window.alert("Bem vindo ao meu curriculo")
--- 03_AUT_EST_ENTREGA/Semana 7/Backend/app_71.js
+++ 03_AUT_EST_ENTREGA/Semana 7/Backend/app_71.js
@@ -22,6 +22,53 @@ app.get('/info', (req, res) => {
 	});
 	db.close(); // Fecha o banco
 });
+// Insere um registro (é o C do CRUD - Create)
+app.post('/userinsert', urlencodedParser, (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+    sql = "INSERT INTO Info (Nome, email, Cargo) VALUES ('Henrique', 'usdhauhdus@gmai.com', 'Estagiario')";
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    db.run(sql, [],  err => {
+        if (err) {
+            throw err;
+        }
+    });
+    db.close(); // Fecha o banco
+    res.end();
+});
+
+// Atualiza um registro (é o U do CRUD - Update)
+app.post('/userupdate', urlencodedParser, (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+    sql = "UPDATE tbUser SET title = '" + req.body.title + "' WHERE userId = " + req.body.userId;
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    db.run(sql, [],  err => {
+        if (err) {
+            throw err;
+        }
+        res.end();
+    });
+    db.close(); // Fecha o banco
+});
+
+// Exclui um registro (é o D do CRUD - Delete)
+app.post('/userdelete', urlencodedParser, (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+    sql = "DELETE FROM tbUser WHERE userId = " + req.body.userId;
+    var db = new sqlite3.Database(DBPATH); // Abre o banco
+    db.run(sql, [],  err => {
+        if (err) {
+            throw err;
+        }
+        res.end();
+    });
+    db.close(); // Fecha o banco
+});
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
